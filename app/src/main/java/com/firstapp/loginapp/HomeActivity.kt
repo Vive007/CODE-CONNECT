613e8b889ae2ab6f728d421aac937dc7db819080
vive007 <vivekpredator007@gmail.com>
2023-12-06 18:10:18 +0530

    successfuly call two api and log there data and image uri

--- app/src/main/java/com/firstapp/loginapp/HomeActivity.kt
+++ app/src/main/java/com/firstapp/loginapp/HomeActivity.kt
@@ -12,6 +12,7 @@ import android.app.Activity
 import android.content.ContentValues.TAG
 import android.net.Uri
 import android.provider.MediaStore
+import android.security.identity.AccessControlProfileId
 import android.util.Log
 import android.widget.ImageView
 import com.google.android.material.tabs.TabLayout.TabGravity
@@ -24,6 +25,7 @@ import retrofit2.http.Tag
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
+import retrofit2.Retrofit
 
 class HomeActivity : AppCompatActivity() {
     private val TAG:String="CHECK_RESPONSE"
@@ -53,6 +55,23 @@ class HomeActivity : AppCompatActivity() {
             val firebaseUser = FirebaseAuth.getInstance().currentUser
             val uid = firebaseUser?.uid
             Log.i(TAG, "UniqueID: ${uid}")// getting UniqueID of user
+
+            // if this uid present in data base uid-> present in profile table means rgisterd user
+
+
+            // don't move to create profile i have to  call the api wwth therir coding id
+
+
+
+
+
+
+
+
+
+
+
+
 
             // Use the retrieved information as needed
             // ...
@@ -110,11 +129,24 @@ class HomeActivity : AppCompatActivity() {
 
         if (requestCode == PROFILE_REQUEST_CODE && resultCode == RESULT_OK) {
             // Handle the result from ProfileActivity
-            val name = data?.getStringExtra("NAME")
+            val name = data?.getStringExtra("NAME")?.lowercase()
             val codingProfile = data?.getStringExtra("CODING_PROFILE")
 
             if (codingProfile != null) {
-                getData(codingProfile)
+                // if name is codeforces then call getdata
+                if(name=="codeforces")
+                {
+                    getData(codingProfile);
+                }else if(name=="leetcode")
+                {
+                    getLeetcodeData(codingProfile)
+
+                }else
+                {
+                    Log.i(TAG, "error message: Your coding profile not found")
+                }
+//                getData(codingProfile)
+//                getLeetcodeData(codingProfile)
             }
         } else if (requestCode == PROFILE_REQUEST_CODE && resultCode == Activity.RESULT_CANCELED) {
             // Handle if the user canceled the operation (e.g., pressing the back button)
@@ -137,9 +169,62 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private fun getLeetcodeData(profileId:  String) {
+        RetrofitLeetcode.leetCodeApiInterface.getData(profileId)
+            .enqueue(object : Callback<ResponseDataLeetcodeClass?> {
+                override fun onResponse(
+                    call: Call<ResponseDataLeetcodeClass?>,
+                    response: Response<ResponseDataLeetcodeClass?>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { responseData ->
+                            // Display the data
+                            logLeetCodeData(responseData)
+                        }
+                    } else {
+                        // Handle unsuccessful response (e.g., show an error message)
+                        Log.e(TAG, "Unsuccessful response: ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseDataLeetcodeClass?>, t: Throwable) {
+                    // Handle failure (e.g., show an error message)
+                    Log.e(TAG, "Request failed", t)
+                }
+            })
 
 
-            // Display the name and coding profile in the Home page
+    }
+
+    private fun logLeetCodeData(responseData: ResponseDataLeetcodeClass) {
+        Log.i(TAG, "contributionPoint: ${responseData.contributionPoint}")
+        Log.i(TAG, "easySolved: ${responseData.easySolved}")
+        Log.i(TAG, "hardSolved: ${responseData.hardSolved}")
+        Log.i(TAG, "mediumSolved: ${responseData.mediumSolved}")
+        Log.i(TAG, "ranking: ${responseData.ranking}")
+        Log.i(TAG, "reputation: ${responseData.reputation}")
+
+        // Log submissionCalendar data
+        Log.i(TAG, "submissionCalendar: ${responseData.submissionCalendar}")
+
+        Log.i(TAG, "totalEasy: ${responseData.totalEasy}")
+        Log.i(TAG, "totalHard: ${responseData.totalHard}")
+        Log.i(TAG, "totalMedium: ${responseData.totalMedium}")
+        Log.i(TAG, "totalQuestions: ${responseData.totalQuestions}")
+        Log.i(TAG, "totalSolved: ${responseData.totalSolved}")
+
+        // Log totalSubmissions list
+        for (submission in responseData.totalSubmissions) {
+            Log.i(TAG, "Submission - difficulty: ${submission.difficulty}")
+            Log.i(TAG, "Submission - count: ${submission.count}")
+            Log.i(TAG, "Submission - submissions: ${submission.submissions}")
+        }
+
+
+    }
+
+
+    // Display the name and coding profile in the Home page
 //            val welcomeMessageTextView = findViewById<TextView>(R.id.messageTextView)
 //            val codingProfileTextView = findViewById<TextView>(R.id.messageTextView2)
 //
@@ -199,14 +284,14 @@ class HomeActivity : AppCompatActivity() {
 //                               .error(R.drawable.baseline_person_24) // Error image if loading fails
 //                               .into(findViewById(R.id.createProfileButton))
 
-                           // Set the text for dynamically created TextViews
-                           findViewById<TextView>(R.id.firstNameTextView).text = "First Name: ${firstResult.firstName}"
-                           findViewById<TextView>(R.id.lastNameTextView).text = "Last Name: ${firstResult.lastName}"
-                           findViewById<TextView>(R.id.handleTextView).text = "Handle: ${firstResult.handle}"
-                           findViewById<TextView>(R.id.ratingTextView).text = "Rating: ${firstResult.rating}"
-                           findViewById<TextView>(R.id.rankTextView).text = "Rank: ${firstResult.rank}"
-                           findViewById<TextView>(R.id.maxratingTextView).text = "MaxRating: ${firstResult.maxRating}"
-                           findViewById<TextView>(R.id.maxrankTextView).text = "MaxRank: ${firstResult.maxRank}"
+//                           // Set the text for dynamically created TextViews
+//                           findViewById<TextView>(R.id.firstNameTextView).text = "First Name: ${firstResult.firstName}"
+//                           findViewById<TextView>(R.id.lastNameTextView).text = "Last Name: ${firstResult.lastName}"
+//                           findViewById<TextView>(R.id.handleTextView).text = "Handle: ${firstResult.handle}"
+//                           findViewById<TextView>(R.id.ratingTextView).text = "Rating: ${firstResult.rating}"
+//                           findViewById<TextView>(R.id.rankTextView).text = "Rank: ${firstResult.rank}"
+//                           findViewById<TextView>(R.id.maxratingTextView).text = "MaxRating: ${firstResult.maxRating}"
+//                           findViewById<TextView>(R.id.maxrankTextView).text = "MaxRank: ${firstResult.maxRank}"
 
                            // Set more TextViews for other properties as needed
                        }
